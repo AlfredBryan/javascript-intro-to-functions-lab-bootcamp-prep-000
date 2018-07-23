15acee1e11030538deeabf2bb4f094fbe2a7daad
Chimereze Ikenna-Alfred <alfred.chimereze@gmail.com>
2018-07-23 08:21:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,33 @@
+var lowercase = 'hello!'
+var uppercase = "HELLO!"
+
+function shout(String) {
+ return 'Hello'.toUpperCase(); 
+}
+
+shout('hello');
+
+function whisper(String) {
+  return 'HELLO'.toLowerCase();
+}
+whisper('HELLO');
+
+function logShout(String) {
+  console.log('Hello'.toUpperCase());
+}
+logShout('hello');
+
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase());
+}
+logWhisper('HELLO');
+
+function sayHiToGrandma(string) {
+  return 'hello'
+  if (string === lowercase ) {
+    return "I can\'t hear you!"
+  } else {
+    return "YES INDEED!"
+  }
+}
+sayHiToGrandma('hello')
